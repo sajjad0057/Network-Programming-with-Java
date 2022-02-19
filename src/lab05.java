01403728d1913be135cad8e28f50ba27878a1808
sajjad0057 <sajjadhossain0057@gmail.com>
2022-02-20 05:43:25 +0600

    add new

--- src/lab05.java
+++ src/lab05.java
@@ -12,6 +12,22 @@ public class lab05 extends Thread{
 
     public static void main(String[] args) {
         new lab05().start();
+        new ThreadTwo().start();
         System.out.println("I am Main");
+    }
+}
+
+
+
+class ThreadTwo extends Thread {
+    @Override
+    public void run() {
+        try {
+            Thread.sleep(2000);
+            System.out.println("ThreadTwo executed !");
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        System.out.println();
     }
 }
--- src/lab06.java
+++ src/lab06.java
@@ -2,51 +2,51 @@ import java.io.*;
 import java.net.ServerSocket;
 import java.net.Socket;
 
-//public class lab06 {
-//
-//    public static void main(String[] args) {
-//        new Server().start();
-//        new Client().start();
-//        System.out.println("lab 06");
-//    }
-//}
+public class lab06 {
 
-//class Server extends Thread{
-//    @Override
-//    public void run() {
-//        ServerSocket ss= null;
-//        try {
-//            ss = new ServerSocket(5000);
-//            System.out.println("Server Started");
-//            Socket socket=ss.accept();
-//            System.out.println("Received a client");
-//            DataInputStream bif=new DataInputStream(socket.getInputStream());
-//            System.out.println(bif.readUTF());
-//        } catch (IOException e) {
-//            e.printStackTrace();
-//        }
-//    }
-//}
+    public static void main(String[] args) {
+        new Server().start();
+        new Client().start();
+        System.out.println("lab 06");
+    }
+}
 
-//class Client extends Thread{
-//
-//
-//    @Override
-//    public void run() {
-//        try {
-//            Thread.sleep(5000);
-//        } catch (InterruptedException e) {
-//            e.printStackTrace();
-//        }
-//        Socket socket= null;
-//        try {
-//            socket = new Socket("localhost",5000);
-//            DataOutputStream bof=new DataOutputStream(socket.getOutputStream());
-//
-//            bof.writeUTF("hello Server");
-//            bof.flush();
-//        } catch (IOException e) {
-//            e.printStackTrace();
-//        }
-//    }
-//}
+class Server extends Thread{
+    @Override
+    public void run() {
+        ServerSocket ss= null;
+        try {
+            ss = new ServerSocket(5000);
+            System.out.println("Server Started");
+            Socket socket=ss.accept();
+            System.out.println("Received a client");
+            DataInputStream bif=new DataInputStream(socket.getInputStream());
+            System.out.println(bif.readUTF());
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+}
+
+class Client extends Thread{
+
+
+    @Override
+    public void run() {
+        try {
+            Thread.sleep(5000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        Socket socket= null;
+        try {
+            socket = new Socket("localhost",5000);
+            DataOutputStream bof=new DataOutputStream(socket.getOutputStream());
+
+            bof.writeUTF("hello Server");
+            bof.flush();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+}
